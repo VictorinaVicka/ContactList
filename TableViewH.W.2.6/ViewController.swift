c4a17f58e14d4c7a951825566bea750340db88e2
VictorinaVicka <city_br@mail.ru>
2019-10-04 12:51:46 +0300

    Initial Commit

---
+++ TableViewH.W.2.6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewH.W.2.6
+//
+//  Created by Виктория Воробьева on 04/10/2019.
+//  Copyright © 2019 Виктория Воробьева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
